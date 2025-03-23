bc85100c89e4bf663476cdfc29f23b03033df2e8
yoon664 <yoonj0264@gmail.com>
2025-03-23 10:49:44 +0900

    a

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Step01 from './step/step01';
 import Step02 from './step/step02';
 import Step03 from './step/step03';
 import Step04 from './step/step04';
+import Step05 from './step/step05';
 
 function App() {
   return (
     // <Step01 />
     // <Step02 />
     // <Step03 />
-    <Step04 />
+    // <Step04 />
+    <Step05 />
   );
 }
 
---
+++ src/step/step05.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/*
+    [ useEffect ]
+    컴포넌트가 랜더링 될때마다 특정 작업을 수행하도록 할 수 있는 HOOK
+*/
+
+const Step05 = () => {
+  return (
+    <div>Step05</div>
+  )
+}
+
+export default Step05
